ff6511fcc4c4e423971449393239bfd0d31620d6
puterabot <putera.ai.bot@gmail.com>
2024-01-01 15:31:19 +0100

    Script to verify test results

---
+++ scripts/testReviewResults.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+findimagedupes -t 100 `ls doc/testBaseImages/*.ppm output/*.ppm | sort` 2> /dev/null | sort > /tmp/x
+
+PATTERN=`pwd -P`/
+
+N=`awk -v pattern="$PATTERN" '{gsub(pattern, ""); print}' /tmp/x | wc -l`
+
+if [ "$N" -eq 12 ]; then
+    echo "TEST PASS"
+else
+    echo "*** TEST ERROR! ***"
+fi
