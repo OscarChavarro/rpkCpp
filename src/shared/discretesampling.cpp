4bf540300012a9bd4795a978715f117be6f117ad
Oscar Chavarro <jedilink@gmail.com>
2024-01-31 22:14:18 +0100

    Clean up: removing unused sampling

--- src/shared/discretesampling.cpp
+++ src/shared/discretesampling.cpp
@@ -1,27 +1,6 @@
 #include "shared/discretesampling.h"
 
 int
-SampleDiscrete(const float probabilities[], float total, double *x_1, double *pdf) {
-    int i = 0;
-    float sum, left;
-    double sample = *x_1 * total;
-
-    sum = probabilities[0];
-
-    while ( sample > sum ) {
-        i++;
-        sum += probabilities[i];
-    }
-
-    /* Rescale x_1 */
-    left = sum - probabilities[i];
-    *x_1 = ((sample - left) / (sum - left));
-    *pdf = probabilities[i] / total;
-
-    return i;
-}
-
-int
 DSampleDiscrete(const double probabilities[], double total, double *x_1, double *pdf) {
     int i = 0;
     double sum, left;
--- src/shared/discretesampling.h
+++ src/shared/discretesampling.h
@@ -1,10 +1,6 @@
 #ifndef _SAMPLING_H_
 #define _SAMPLING_H_
 
-/* Sample from a discrete pdf. total is the sum of the
-   (non scaled) probabilities, x_1 is re-adjusted to [0,1] after
-   sampling, pdf for choosing index is filled in */
-int SampleDiscrete(const float probabilities[], float total, double *x_1, double *pdf);
 int DSampleDiscrete(const double probabilities[], double total, double *x_1, double *pdf);
 
 #endif
