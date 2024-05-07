977b62e35ca80760945aec4339daa27fd5d9a430
puterabot <putera.ai.bot@gmail.com>
2024-05-07 17:46:13 +0200

    Refactor: making fields private (Texture)

--- src/material/Texture.h
+++ src/material/Texture.h
@@ -4,11 +4,13 @@
 #include "common/ColorRgb.h"
 
 class Texture {
-  public:
+  private:
     int width;
     int height;
     int channels;
     unsigned char *data; // First bytes correspond to bottom-left pixel (as in OpenGL)
+
+  public:
     ColorRgb evaluateColor(float u, float v) const;
 };
 
